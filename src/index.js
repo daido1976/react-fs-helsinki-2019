74f94f0b3b42bb889ffdb4c732eec9724e6bcf4e
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-07-20 23:37:41 +0900

    Rename src/Counter.js to src/part1/Counter.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import ReactDOM from "react-dom";
-import Counter from "./Counter";
+import Counter from "./part1/Counter";
 
 const App = () => {
   return (
