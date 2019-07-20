8a58bdbc53440cda9a8e5fa472b8563dcec47c67
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-07-20 22:50:13 +0900

    Create Display component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Display = ({ counter }) => {
+  return <div>{counter}</div>;
+};
+
 const App = () => {
   const [counter, setCounter] = useState(0);
 
@@ -8,7 +12,7 @@ const App = () => {
 
   return (
     <div>
-      <div>{counter}</div>
+      <Display counter={counter} />
       <button onClick={setToValue(counter + 1)}>plus</button>
       <button onClick={setToValue(0)}>zero</button>
     </div>
