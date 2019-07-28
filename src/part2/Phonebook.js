79af9410caa71a35413e991c129ec480fda6b4ba
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-07-28 12:32:03 +0900

    Step1 Impl Row, rows, handleNameChange, addName

--- src/part2/Phonebook.js
+++ src/part2/Phonebook.js
@@ -1,22 +1,45 @@
 import React, { useState } from "react";
 
+const Row = props => {
+  return <li>{props.person.name}</li>;
+};
+
 const Phonebook = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const rows = () => {
+    return persons.map((person, index) => <Row key={index} person={person} />);
+  };
+
+  const handleNameChange = event => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const addName = event => {
+    event.preventDefault();
+    const personObject = {
+      name: newName
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>{rows()}</ul>
     </div>
   );
 };
