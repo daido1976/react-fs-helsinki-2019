60d4b395df3b9d64cca19f92271922a3f42b8e98
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-07-28 09:19:07 +0900

    Impl addNote & handleNoteChange

--- src/part2/Form.js
+++ src/part2/Form.js
@@ -4,13 +4,28 @@ import Note from "./Note";
 
 const Form = props => {
   const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
 
   const rows = () => notes.map(note => <Note key={note.id} note={note} />);
+
+  const addNote = event => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+  };
+
+  const handleNoteChange = event => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>{rows()}</ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
