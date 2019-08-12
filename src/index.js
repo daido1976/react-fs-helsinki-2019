d816e664906ea90f3b60012ebb37f395984f59b9
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-08-13 02:31:35 +0900

    Fix warning & refactor in graphql app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloProvider } from "react-apollo";
-import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
-import { GraphqlClient, client } from "./part8/graphql/graphqlClient";
 import { GraphqlApp } from "./part8/graphql";
 
 const App = () => {
--- src/part8/graphql/index.js
+++ src/part8/graphql/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { ApolloProvider } from "react-apollo";
 import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
 import { GraphqlClient, client } from "./graphqlClient";
