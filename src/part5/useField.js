7d369f0dd8cf8378c1eef2f97776abd55e42288a
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-08-17 22:42:41 +0900

    Impl useField

--- src/part5/useField.js
+++ src/part5/useField.js
@@ -1,36 +1,42 @@
 import React, { useState } from "react";
 
+const useField = type => {
+  const [value, setValue] = useState("");
+
+  const onChange = event => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange
+  };
+};
+
 export const Field = () => {
-  const [name, setName] = useState("");
-  const [born, setBorn] = useState("");
-  const [height, setHeight] = useState("");
+  const name = useField("text");
+  const born = useField("date");
+  const height = useField("number");
 
   return (
     <div>
       <form>
         name:
-        <input
-          type="text"
-          value={name}
-          onChange={event => setName(event.target.value)}
-        />
+        <input type={name.type} value={name.value} onChange={name.onChange} />
         <br />
         birthdate:
-        <input
-          type="date"
-          value={born}
-          onChange={event => setBorn(event.target.value)}
-        />
+        <input type={born.type} value={born.value} onChange={born.onChange} />
         <br />
         height:
         <input
-          type="number"
-          value={height}
-          onChange={event => setHeight(event.target.value)}
+          type={height.type}
+          value={height.value}
+          onChange={height.onChange}
         />
       </form>
       <div>
-        {name} {born} {height}
+        {name.value} {born.value} {height.value}
       </div>
     </div>
   );
