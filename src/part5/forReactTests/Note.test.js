a4810196bc5996e18072d9fc68f92c47979c0d92
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-08-18 14:33:36 +0900

    Searching for content in a component
    $ CI=true yarn test

--- src/part5/forReactTests/Note.test.js
+++ src/part5/forReactTests/Note.test.js
@@ -16,4 +16,14 @@ test("renders content", () => {
   expect(component.container).toHaveTextContent(
     "Component testing is done with react-testing-library"
   );
+
+  const element = component.getByText(
+    "Component testing is done with react-testing-library"
+  );
+  expect(element).toBeDefined;
+
+  const div = component.container.querySelector(".note");
+  expect(div).toHaveTextContent(
+    "Component testing is done with react-testing-library"
+  );
 });
