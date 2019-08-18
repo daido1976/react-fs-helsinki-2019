37904491ff5b32049c1d13b695bbdfd14da12e64
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-08-18 14:49:15 +0900

    Clicking buttons in tests
    https://jestjs.io/docs/en/mock-functions.html
    https://testing-library.com/docs/dom-testing-library/api-events#fireevent

--- src/part5/forReactTests/Note.test.js
+++ src/part5/forReactTests/Note.test.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-undef */
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render, cleanup } from "@testing-library/react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
 import { Note } from "./Note";
 
 afterEach(cleanup);
@@ -26,4 +27,22 @@ test("renders content", () => {
   expect(div).toHaveTextContent(
     "Component testing is done with react-testing-library"
   );
+});
+
+test("clicking the button calls event handler once", async () => {
+  const note = {
+    content: "Component testing is done with react-testing-library",
+    important: true
+  };
+
+  const mockHandler = jest.fn();
+
+  const { getByText } = render(
+    <Note note={note} toggleImportance={mockHandler} />
+  );
+
+  const button = getByText("make not important");
+  fireEvent.click(button);
+
+  expect(mockHandler.mock.calls.length).toBe(1);
 });
